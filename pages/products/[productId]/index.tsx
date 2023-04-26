664ee8660cd25efc6eb7780bcaf651c7f2202c9f
Prince Narteh <prinarteh@gmail.com>
2023-04-26 10:36:21 +0000

    fix product details

--- pages/products/[productId]/index.tsx
+++ pages/products/[productId]/index.tsx
@@ -1,15 +1,31 @@
 import { Back, Card, Loader } from "@/components";
-import { getProduct } from "@/services/products";
+import { axiosAuth } from "@/lib/axios";
+import { allProducts, getProduct } from "@/services/products";
 import { Product } from "@/types/product";
 import { capitalize } from "@/utils/utilities";
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 import { useEffect, useState } from "react";
 
-const ProductDetails = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { productId } = context.query;
+  const data = await getProduct(productId as string);
+
+  return {
+    props: {
+      product: data,
+    },
+  };
+};
+
+const ProductDetails = ({ product }: { product: Product }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<Product | null>(null);
+
+  console.log(product);
 
   const {
     query: { productId },
@@ -19,27 +35,16 @@ const ProductDetails = () => {
     return <Loader />;
   }
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      setIsLoading(true);
-      const res = await getProduct(productId as string);
-      setData(res.data);
-      setIsLoading(false);
-    };
-
-    fetchProduct();
-  }, []);
-
   return (
     <div className="mx-auto w-11/12 space-y-3 pb-5">
       <Back />
       <Card heading="Product Detail">
-        <h3 className="mb-2 text-2xl font-semibold">{data?.title}</h3>
+        <h3 className="mb-2 text-2xl font-semibold">{product?.title}</h3>
         <div className="min-h-96 grid grid-cols-1 space-y-5 md:grid-cols-12">
           <div className="col-span-5 space-y-3">
             <div className="relative h-[400px] bg-slate-500">
               <Image
-                src={data?.images[0]?.secure_url as string}
+                src={product?.images[0]?.secure_url as string}
                 sizes="400"
                 fill
                 style={{ objectFit: "contain" }}
@@ -48,7 +53,7 @@ const ProductDetails = () => {
               />
             </div>
             <div className="flex justify-between gap-3 overflow-x-auto">
-              {data?.images.map((image, idx) => (
+              {product?.images.map((image, idx) => (
                 <div
                   key={idx}
                   className="relative h-[100px] w-[100px] shrink-0"
@@ -65,22 +70,22 @@ const ProductDetails = () => {
             </div>
           </div>
           <div className="col-span-7 px-5">
-            <Item label="Price" value={`¢${data?.price || ""}`} dark />
+            <Item label="Price" value={`¢${product?.price || ""}`} dark />
             <Item
               label="Discount Percentage"
-              value={`¢${data?.discountPercentage || ""}`}
+              value={`¢${product?.discountPercentage || ""}`}
             />
-            <Item label="Stock" value={`${data?.stock || ""}`} dark />
-            <Item label="Brand" value={`${(data?.brand as string) || ""}`} />
+            <Item label="Stock" value={`${product?.stock || ""}`} dark />
+            <Item label="Brand" value={`${(product?.brand as string) || ""}`} />
             <Item
               label="Category"
-              value={`${capitalize((data?.category as string) || "")}`}
+              value={`${capitalize((product?.category as string) || "")}`}
               dark
             />
-            <Item label="Shop" value={`${data?.shopId || ""}`} />
+            <Item label="Shop" value={`${product?.shopId || ""}`} />
             <div className={`bg-dark-gray py-4 px-4`}>
               <div className="mb-3 font-bold">Description</div>
-              <div className="line-clamp-5">{data?.description || ""}</div>
+              <div className="line-clamp-5">{product?.description || ""}</div>
             </div>
           </div>
         </div>

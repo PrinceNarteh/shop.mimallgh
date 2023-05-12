3d37279366990bb75c29ac4aab0f6b47e8e59e41
Prince Narteh <prinarteh@gmail.com>
2023-05-12 12:52:57 +0000

    fix env variables

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://api.mimallgh.com",
   headers: { "Content-Type": "application/json" },
 });
 
 export const axiosAuth = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://api.mimallgh.com",
   headers: { "Content-Type": "application/json" },
 });
--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["placeimg.com", "res.cloudinary.com", "loremflickr.com"],
   },
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 };
 
 module.exports = nextConfig;
